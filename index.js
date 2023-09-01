08d0278baa8ec414c4ef2cd553bb3f250821f865
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-01 15:14:43 +0800

    some error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function chat() {
     var know = {
-        "who are you?": "Hi Im am your personal chat bot! Pixie,<br> What is your name master?",
-        "Frank": "Hello master Frank!<br> How may I help you?",
+        "Who are you?": "Hi Im am your personal chat bot! Pixie,<br> What is your name master?",
+        "Frank": "Hello master!<br> How may I help you?",
         "I want to talk to you": "I'm sorry master Frank <br> I cant talk to you but <br> you call talk to my Brother",
         "How?": "Please click the button below",
         "Ok Thank you, Pixie!": "Your always welcome master Frank"
