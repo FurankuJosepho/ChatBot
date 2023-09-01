7e1f32c407431ddbec35842d68ea7dd07fbe5c38
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-01 14:35:03 +0800

    working bot

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function chat() {
     var know = {
         "who are you?": "Hi Im am your personal chat bot! Pixie,<br> What is your name master?",
         "Frank": "Hello master Frank!<br> How may I help you?",
-        
+        "I want to talk to you": "I'm sorry master Frank <br> I cant talk to you but <br> you call talk to my Brother",
+        "How?": "Please click the button below",
+        "Ok Thank you, Pixie!": "Your always welcome master Frank"
     }
     var user = document.getElementById("userSpace").value;
     document.getElementById("botResponse").innerHTML = user + "<br>";
@@ -14,3 +16,10 @@ function chat() {
         "sorry, i didnt understand <br>"
     }
 }
+
+function ashBourne() {
+    alert ("Hello, Master Frank Im Ashbourne your personal chatbot");
+
+    const yourAnswers = prompt ("How may I help you Master Frank?")
+}
+document.getElementById("Ashborn").onclick = ashBourne;
