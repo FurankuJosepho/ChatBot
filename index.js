ac2d6bbc696a9e9a9836fb47babab2dec8b2ea66
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-01 14:49:28 +0800

    finish product

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ function chat() {
 function ashBourne() {
     alert ("Hello, Master Frank Im Ashbourne your personal chatbot");
 
-    const yourAnswers = prompt ("How may I help you Master Frank?")
+    const yourAnswers = prompt ("How may I help you Master Frank?");
+    if (yourAnswers == "I want to talk") {
+        var talkBot = prompt ("What do you want to talk about Master?");
+        if (talkBot == "Relationship") {
+            var talk = prompt ("Hows your relationship with other?")
+            if (talk == "not good", "so bad", "bad", "not as good it should be") {
+                alert ("Communication, respect, trust, quality time, independence, and effective conflict resolution are essential components of a healthy and fulfilling relationship.")
+            } else if (talk = "good", "not bad", "not so bad", "the best") {
+                alert ("Keep up on having a good relationship with others Master Frank I your personal chat bot commended you for being the best person!")
+            }
+        } else {
+            alert ("Thank you for coming Master Frank!")
+        }
+    } else {
+        alert ("Thank you for coming Master Frank!")
+    }
 }
 document.getElementById("Ashborn").onclick = ashBourne;
