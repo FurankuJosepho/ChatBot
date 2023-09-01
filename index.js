698e1221d249604ea0c2659daf2c91a29c4ae6c9
FurankuJosepho <frank21garcia29@gmail.com>
2023-09-01 15:37:04 +0800

    some changes

--- index.js
+++ index.js
@@ -30,12 +30,14 @@ function ashBourne() {
                 alert ("Communication, respect, trust, quality time, independence, and effective conflict resolution are essential components of a healthy and fulfilling relationship.")
             } 
             else if (talk == "Good", "Not bad", "Not so bad", "The best", "Happy Life", "Happy", "Very Happy") {
-                prompt ("Keep up on having a good relationship with others Master Frank I your personal chat bot commended you for being the best person!")
+            var positiveRelation = prompt ("Keep up on having a good relationship with others Master Frank I your personal chat bot commended you for being the best person!")
             }
-        } else {
+        } 
+        else {
             alert ("Thank you for coming Master Frank!")
         }
-    } else {
+    } 
+    else {
         alert ("Thank you for coming Master Frank!")
     }
 }
